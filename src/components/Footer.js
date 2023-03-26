38f74d119153122ba0e3def5a8ca99d4015945d5
Isabela <r_isabela@outlook.com>
2023-03-26 19:56:24 +0100

    adicionando header + adicionando footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,7 +1,50 @@
+import { Box, Container, Link, Typography } from "@mui/material"
+import { makeStyles } from "@mui/styles"
+
+const useStyles = makeStyles(() => ({
+  container: {
+    minWidth: '100%',
+    backgroundColor: '#282828',
+    display: 'flex !important',
+    justifyContent: 'center',
+    padding: '20px 0'
+  },
+
+  navFooter: {
+    minWidth: '65%',
+    margin: 'auto',
+  },
+
+  separator: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#a8a8a8',
+    width: '100%',
+    height: 1,
+    margin: '15px 0'
+  }
+}))
+
 function Footer() {
+  const classes = useStyles()
+
   return (
-    <h1>FOOTER</h1>
-  );
+    <Container className={classes.container} maxWidth="lg">
+      <Box className={classes.navFooter}>
+        <Box display="flex" flexDirection="column">
+          <Link href="#" underline="none" color="white">Carreiras</Link>
+          <Link href="#" underline="none" color="white">Política de privacidade</Link>
+          <Link href="#" underline="none" color="white">Termos & condições</Link>
+        </Box>
+        <Box className={classes.separator} />
+        <Box display="flex" justifyContent={"space-between"}>
+          <Typography color="white">© 2023 Todos os direitos reservados pertencem à Hollow</Typography>
+          <Typography color="white">Política de privacidade</Typography>
+        </Box>
+      </Box>
+    </Container>
+  )
 }
 
 export default Footer
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,67 @@
 
+import { Box, Container, Link } from "@mui/material"
+import { makeStyles } from "@mui/styles"
+import '@fontsource/roboto/500.css'
+const useStyles = makeStyles(() => ({
+  
+  container: {
+    height: '120px',
+    minWidth: '97%',
+    borderRadius: '23px',
+    display: 'flex !important',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '15px 25px',
+  },
+  navBar: {
+    display: 'flex',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    margin: 'auto',
+    minWidth: '65%',
+  },
+  img: {
+    width: '280px',
+    padding: '0 0 0 0px'
+  },
+  navMenu: {
+    display: 'flex',
+    width: '50%',
+    alignItems: 'center',
+    justifyContent: 'space-around',
+    font: '18px Roboto',
+  },
+  login: {
+    backgroundColor: 'black',
+    padding: '15px 50px',
+    borderRadius: '50px',
+  },
+  language: {
+    display: 'flex'
+  }
+}))
+
 function Header() {
+  const classes = useStyles()
 
   return (
-    <h1>HEADER</h1>
+      <Container fixed className={classes.container} maxWidth="lg">
+        <Box className={classes.navBar}>
+          <img id='logo' className={classes.img} alt="" src="hollow_horizontal.svg" />
+          <navMenu className={classes.navMenu}>
+            <Link href="#" underline="none" color="#282828">Como Funciona</Link>
+            <Link href="#" underline="none" color="#282828">Cursos</Link>
+            <Link href="#" underline="none" color="#282828">Sobre nós</Link>
+            <Link href="#" underline="none" color="#282828">Planos</Link>
+            <Link href="#" underline="none" color="#282828">Blog</Link>
+            <Link href="#" underline="none" color="#ffd400" className={classes.login}>Login</Link>
+            <Box className={classes.language}>
+              <img alt="" src="globe.svg" />
+              <Link href="#" underline="none" color="#282828">PT</Link>
+            </Box>
+          </navMenu>
+        </Box>
+      </Container>
   )
 }
-
 export default Header
