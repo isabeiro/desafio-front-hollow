b596e898a307658eef50a7cd7e239ccf2f71ba59
Isabela <r_isabela@outlook.com>
2023-03-26 19:08:08 +0100

    estrutura-inicial

---
+++ src/components/Content.js
@@ -0,0 +1,8 @@
+function Content() {
+
+  return (
+      <h1>CONTENT</h1>
+  )
+}
+
+export default Content
---
+++ src/components/Header.js
@@ -0,0 +1,9 @@
+
+function Header() {
+
+  return (
+    <h1>HEADER</h1>
+  )
+}
+
+export default Header
